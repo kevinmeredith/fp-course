b69ca1354997091b3bf9c8a3f91d1d7d85dc9b7e
Kevin Meredith <kevin.m.meredith@gmail.com>
2018-09-25 09:13:29 -0400

    State#eval, get, put and Functor.

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -38,8 +38,7 @@ exec ::
   State s a
   -> s
   -> s
-exec =
-  error "todo: Course.State#exec"
+exec state s = snd (runState state s)
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
@@ -48,8 +47,7 @@ eval ::
   State s a
   -> s
   -> a
-eval =
-  error "todo: Course.State#eval"
+eval state s = fst (runState state s)
 
 -- | A `State` where the state also distributes into the produced value.
 --
@@ -57,8 +55,8 @@ eval =
 -- (0,0)
 get ::
   State s s
-get =
-  error "todo: Course.State#get"
+get = State (\x -> (x, x))
+
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
@@ -67,8 +65,7 @@ get =
 put ::
   s
   -> State s ()
-put =
-  error "todo: Course.State#put"
+put s = State (\_ -> ((), s))
 
 -- | Implement the `Functor` instance for `State s`.
 --
@@ -79,8 +76,9 @@ instance Functor (State s) where
     (a -> b)
     -> State s a
     -> State s b
-  (<$>) =
-    error "todo: Course.State#(<$>)"
+  (<$>) f st = State $ \s -> case (runState st s) of 
+     (a, b) -> (f a, b)         
+  
 
 -- | Implement the `Applicative` instance for `State s`.
 --
